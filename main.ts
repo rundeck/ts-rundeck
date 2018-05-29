c2594379a0fbef31f3b93f9c16c31d1314d9f501
ProTip <greg.zapp@gmail.com>
2018-05-29 15:34:57 -0500

    Updates.

--- main.ts
+++ main.ts
@@ -5,13 +5,15 @@ import Axios from 'axios'
 
 const TOKEN='QH35qTPQzGRGFzH55KxaDJotzPe2URxt'
 const BASE_URI = 'http://localhost:8080'
-const BASE_API_URI = `${BASE_URI}/api/21`
+const BASE_API_URI = `${BASE_URI}`
 
 async function main() {
     const credProvider = new PasswordCredentialProvider(BASE_URI, 'admin', 'password')
-    const client = new Rundeck(credProvider, BASE_API_URI)
+    const tokenProvider = new TokenCredentialProvider(TOKEN)
+    const client = new Rundeck(tokenProvider, BASE_API_URI)
 
-    console.log(await client.listUsers())
+    const resp = await client.listUsers()
+    console.log(resp)
 }
 
 
@@ -19,8 +21,8 @@ class TokenCredentialProvider implements ServiceClientCredentials {
     constructor(readonly token: string) {}
 
     signRequest(webResource: WebResource) {
-        webResource.headers['X-Rundeck-Auth-Token'] = this.token
-        webResource.headers['accept'] = 'application/json'
+        webResource.headers.set('X-Rundeck-Auth-Token', this.token)
+        // webResource.headers.set('accept', 'application/json')
         return Promise.resolve(webResource)
     }
 }
@@ -34,9 +36,9 @@ class PasswordCredentialProvider implements ServiceClientCredentials {
             maxRedirects: 0,
             validateStatus: c => c >= 300 && c < 400
         })
-        console.log(resp.headers)
-        webResource.headers['cookie'] = resp.headers['set-cookie'][0]
-        webResource.headers['accept'] = 'application/json'
+        console.log(resp)
+        webResource.headers.set('cookie', resp.headers['set-cookie'][0])
+        webResource.headers.set('accept', 'application/json')
         console.log(webResource.headers)
         return webResource
     }

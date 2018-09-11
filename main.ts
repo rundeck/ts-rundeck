33bfcb42cd671dc21fd380a8156584c21a2f8c4a
ProTip <greg.zapp@gmail.com>
2018-09-11 14:59:23 -0500

    Update autorest and fix breakage
    Add project archive import

--- main.ts
+++ main.ts
@@ -1,19 +1,24 @@
+import fs from 'fs'
+
 import {Rundeck} from './lib/rundeck'
 import { ServiceClientCredentials, WebResource } from 'ms-rest-js'
 
 import Axios from 'axios'
 
 const TOKEN='QH35qTPQzGRGFzH55KxaDJotzPe2URxt'
-const BASE_URI = 'http://localhost:8080'
+const BASE_URI = 'http://127.0.0.1:4440'
 const BASE_API_URI = `${BASE_URI}`
 
 async function main() {
-    const credProvider = new PasswordCredentialProvider(BASE_URI, 'admin', 'password')
+    const credProvider = new PasswordCredentialProvider(BASE_URI, 'admin', 'admin')
     const tokenProvider = new TokenCredentialProvider(TOKEN)
-    const client = new Rundeck(tokenProvider, BASE_API_URI)
+    const client = new Rundeck(credProvider, BASE_API_URI, {withCredentials: true})
 
     const resp = await client.listUsers()
-    console.log(resp)
+    console.log([...resp])
+
+    const res = await client.importProjectArchive('New', fs.readFileSync('/home/greg/Downloads/Sleep-20180911-140313.rdproject.jar'))
+    
 }
 
 
@@ -32,14 +37,12 @@ class PasswordCredentialProvider implements ServiceClientCredentials {
 
     async signRequest(webResource: WebResource) {
         const {username, password} = this
-        const resp = await Axios.get(`${this.baseUri}/j_security_check?j_username=${username}&j_password=${password}`,{
+        const resp = await Axios.post(`${this.baseUri}/j_security_check?j_username=${username}&j_password=${password}`, null, {
             maxRedirects: 0,
             validateStatus: c => c >= 300 && c < 400
         })
-        console.log(resp)
-        webResource.headers.set('cookie', resp.headers['set-cookie'][0])
+        webResource.headers.set('cookie', resp.headers['set-cookie'])
         webResource.headers.set('accept', 'application/json')
-        console.log(webResource.headers)
         return webResource
     }
 }

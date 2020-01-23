fbe939e862a678ee63df029f0de1ca22ec9529b9
ProTip <greg.zapp@gmail.com>
2020-01-23 16:07:00 -0600

    Cache preflight login promise
    Uses one Rundeck session per client instantiation

--- src/passwordCredProvider.ts
+++ src/passwordCredProvider.ts
@@ -2,17 +2,30 @@ import {WebResource} from '@azure/ms-rest-js'
 
 import {BaseCredentialProvider} from './baseCredProvider'
 
-import Axios from 'axios'
+import Axios, { AxiosResponse } from 'axios'
 
 export class PasswordCredentialProvider extends BaseCredentialProvider {
+    loginRequest?: Promise<AxiosResponse<any>>
+
     constructor(readonly baseUri: string, readonly username: string, readonly password: string) {
         super()
     }
 
     async signRequest(webResource: WebResource) {
         await super.signRequest(webResource)
+        
+        if (! this.loginRequest)
+            this.loginRequest = this.login()
+
+        const resp = await this.loginRequest
+
+        webResource.headers.set('cookie', resp.headers['set-cookie'].join(';'))
+        return webResource
+    }
+
+    async login() {
         const {username, password} = this
-        const resp = await Axios.post(`${this.baseUri}/j_security_check`, null, {
+        return await Axios.post(`${this.baseUri}/j_security_check`, null, {
             params: {
                 'j_username': username,
                 'j_password': password,
@@ -20,7 +33,5 @@ export class PasswordCredentialProvider extends BaseCredentialProvider {
             maxRedirects: 0,
             validateStatus: c => c >= 300 && c < 400
         })
-        webResource.headers.set('cookie', resp.headers['set-cookie'].join(';'))
-        return webResource
     }
 }

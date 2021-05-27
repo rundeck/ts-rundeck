6f89b7ca0825d7f325f6d224ee15d879c043775a
ProTip <greg.zapp@gmail.com>
2021-05-27 16:40:55 +0000

    Use current implementation instead of super
    Release 0.2.3

--- src/RundeckClient.ts
+++ src/RundeckClient.ts
@@ -5,6 +5,6 @@ import { RequestPrepareOptions, HttpOperationResponse } from "@azure/ms-rest-js"
 export class RundeckClient extends Rundeck {
     apiRequest(options: RequestPrepareOptions): Promise<HttpOperationResponse> {
         options.baseUrl = this.baseUri
-        return super.sendRequest(options)
+        return this.sendRequest(options)
     }
 }
